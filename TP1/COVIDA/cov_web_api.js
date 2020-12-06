b759607b3cf9084111dcd7001f5edfa45a2ea3bf
unknown <landroskill2@gmail.com>
2020-12-06 17:35:09 +0000

    Actual final product

--- TP1/COVIDA/cov_web_api.js
+++ TP1/COVIDA/cov_web_api.js
@@ -16,7 +16,7 @@ module.exports = function (covServices) {
     }
     
     function searchGames(req, rsp){
-        if(!req.query.name) return rsp.status(422).json(new Error(`Invalid query syntax, please make sure query params are according to the documentation.`), req.originalUrl)
+        if(!req.query.name) return rsp.status(422).json(new Error(`Invalid query syntax, please make sure query params are according to the documentation.`, req.originalUrl))
         covServices.searchGames(req.query.name, (err, games, status) => { 
             if(err == null) sendSuccess(req,rsp,new Answer(`Displaying search results`,JSON.parse(games)),200)
             else rsp.status(status).json(new Error(err.message, req.originalUrl))
@@ -31,7 +31,7 @@ module.exports = function (covServices) {
       })
   }
   function editGroup(req, rsp){
-      if(!req.body.newName && !req.body.newDesc) return rsp.status(422).json(new Error(`Invalid body syntax, please make sure body params are according to the documentation.`), req.originalUrl)
+      if(!req.body.newName && !req.body.newDesc) return rsp.status(422).json(new Error(`Invalid body syntax, please make sure body params are according to the documentation.`, req.originalUrl))
       const groupName = req.params.groupName.split("+").join(" ")
       covServices.editGroup(groupName, req.body.newName, req.body.newDesc, (err, succ, status) => {
           if(err == null) sendSuccess(req,rsp,new Answer(`Group sucessfully edited.`,succ),200)
@@ -56,7 +56,7 @@ module.exports = function (covServices) {
   }
 
   function addToGroup(req, rsp){
-      if(!req.body.gameID) return rsp.status(422).json(new Error(`Invalid body syntax - cannot find gameID - please make sure body params are according to the documentation.`), req.originalUrl)
+      if(!req.body.gameID) return rsp.status(422).json(new Error(`Invalid body syntax - cannot find gameID - please make sure body params are according to the documentation.`, req.originalUrl))
       if(isNaN(req.body.gameID)) return rsp.status(406).json(new Error(`${req.body.gameID} is not a valid ID. ID's must consist only of numbers.`), req.originalUrl)
       const groupName = req.params.groupName.split("+").join(" ")
       covServices.addToGroup(req.body.gameID, groupName, (err, game, status) => {
@@ -66,7 +66,7 @@ module.exports = function (covServices) {
   }
 
   function removeFromGroup(req, rsp){
-      if(!req.body.gameID) return rsp.status(422).json(new Error(`Invalid body syntax - cannot find gameID - please make sure body params are according to the documentation.`), req.originalUrl)
+      if(!req.body.gameID) return rsp.status(422).json(new Error(`Invalid body syntax - cannot find gameID - please make sure body params are according to the documentation.`, req.originalUrl))
       if(isNaN(req.body.gameID)) return rsp.status(406).json(new Error(`${req.body.gameID} is not a valid ID. ID's must consist only of numbers.`),req.originalUrl)
       const groupName = req.params.groupName.split("+").join(" ")
       covServices.removeFromGroup(req.body.gameID, groupName, (err, game, status) => {
@@ -76,7 +76,7 @@ module.exports = function (covServices) {
   }
 
   function getGamesBetween(req, rsp){
-      if(req.query.minRating > req.query.maxRating) return rsp.status(406).json(new Error(`${req.query.minRating} to ${req.query.maxRating} is not a valid interval`), req.originalUrl)
+      if(req.query.minRating > req.query.maxRating) return rsp.status(406).json(new Error(`${req.query.minRating} to ${req.query.maxRating} is not a valid interval`, req.originalUrl))
       const groupName = req.params.groupName.split("+").join(" ")
       covServices.getGamesBetween(groupName, req.query.maxRating, req.query.minRating, (err, succ, status) => {
           if(err == null) sendSuccess(req,rsp,new Answer('Games that fit interval:',succ),200)
